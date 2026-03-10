1cf23f770b2bd120f31295dab864746f854d1648
sindhusaravv <sindhusaravanan06@gmail.com>
2026-03-10 10:24:42 +0530

    UC1:1HotelBookingApp

--- src/Bookmystayapp.java
+++ src/Bookmystayapp.java
@@ -1,64 +1,7 @@
-import java.util.LinkedList;
-import java.util.Queue;
+ class UseCase1HotelBookingApp {
+    public static void main(String[]args){
 
-class Reservation {
-    String guestName;
-    String roomType;
-
-    Reservation(String guestName, String roomType) {
-        this.guestName = guestName;
-        this.roomType = roomType;
-    }
-
-    void displayReservation() {
-        System.out.println("Guest: " + guestName + " | Requested Room: " + roomType);
-    }
-}
-
-class BookingRequestQueue {
-
-    private Queue<Reservation> requestQueue;
-
-    BookingRequestQueue() {
-        requestQueue = new LinkedList<>();
-    }
-
-    public void addRequest(Reservation reservation) {
-        requestQueue.add(reservation);
-        System.out.println("Booking request added for " + reservation.guestName);
-    }
-
-    public void displayQueue() {
-        System.out.println("\nCurrent Booking Request Queue:");
-        if (requestQueue.isEmpty()) {
-            System.out.println("No booking requests in queue.");
-            return;
-        }
-        for (Reservation r : requestQueue) {
-            r.displayReservation();
-        }
-    }
-}
-
- class UseCase5BookingRequestQueue {
-
-    public static void main(String[] args) {
-
-        System.out.println("=================================");
-        System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking System Version 5.1");
-        System.out.println("=================================\n");
-
-        BookingRequestQueue bookingQueue = new BookingRequestQueue();
-
-        Reservation r1 = new Reservation("Alice", "Single Room");
-        Reservation r2 = new Reservation("Bob", "Double Room");
-        Reservation r3 = new Reservation("Charlie", "Suite Room");
-
-        bookingQueue.addRequest(r1);
-        bookingQueue.addRequest(r2);
-        bookingQueue.addRequest(r3);
-
-        bookingQueue.displayQueue();
+        System.out.println("Welcome to book my stay app");
+        System.out.println("System started successfully ");
     }
 }
